06608042238b5882631d204fd8760043dd71c744
SaiIndra2003 <suryasaiindra@gmail.com>
2023-05-31 16:20:35 +0530

    starter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Home from "./containers/home/index";
+import About from "./containers/about/index";
+import Portfolio from "./containers/portfolio/index";
+import Resume from "./containers/resume/index";
+import Skills from "./containers/skills/index";
+import Contact from "./containers/contact/index";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Particles */}
+      {/* Nav bar */}
+      {/* Main page */}
+      <Routes>
+        {/* Create routes */}
+        <Route path="/" index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
     </div>
   );
 }
